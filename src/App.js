0289a8311a3b6407deb51fe883a587fa0f59c0f7
Alex <variuhin75@gmail.com>
2021-02-01 21:12:55 +0300

    Added mock table data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import './App.css';
+import json from './__mock_data__/data.js'
+import {useState, useEffect} from 'react'
+
+const parsedData = JSON.parse(json);
 
 function App() {
+  const [data, setData] = useState(parsedData)
+
+  useEffect(() => {
+  })
+
   return (
     <div className="App">
     </div>
