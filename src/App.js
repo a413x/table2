16c8c32fa9b539292d8898b08bf4c22bd7aeccb3
Alex <variuhin75@gmail.com>
2021-02-02 22:55:50 +0300

    Change table data

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import './styles/App.css';
-import json from './__mock_data__/data.js'
+import {default as json} from './__mock_data__/data.json'
 import {useState} from 'react'
 import {sortData} from './utils/utils.js'
 
 import {Table} from './components/Table.js'
 import {PagesNav} from './components/PagesNav.js'
 
-const parsedData = JSON.parse(json);
 export const rowsOnPage = 20
 
 function App() {
-  const [data, setData] = useState(parsedData)
+  const [data, setData] = useState(json)
   const [page, setPage] = useState(1)
 
   const sortCallback = (col, order) => {
