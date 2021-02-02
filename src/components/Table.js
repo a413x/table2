745cf0da87260f9d868a811a55e44550ae072dde
Alex <variuhin75@gmail.com>
2021-02-02 19:57:37 +0300

    Make table header and first column fixed

--- src/components/Table.js
+++ src/components/Table.js
@@ -3,19 +3,21 @@ import '../styles/Table.css';
 
 export function Table({ data }) {
   return (
-    <table className = 'table'>
-      <TableHead data = {data}/>
-      <tbody>
-        {data.map((dataObj, ind) =>
-          <tr key = {'tr-' + ind}>
-            {colNames.map((colObj, ind) =>
-              <td key = {ind}>
-                { dataObj[colObj.name] }
-              </td>
-            )}
-          </tr>
-        )}
-      </tbody>
-    </table>
+    <div className = 'table-container'>
+      <table className = 'table'>
+        <TableHead data = {data}/>
+        <tbody>
+          {data.map((dataObj, ind) =>
+            <tr key = {'tr-' + ind}>
+              {colNames.map((colObj, ind) =>
+                <td key = {ind}>
+                  { dataObj[colObj.name] }
+                </td>
+              )}
+            </tr>
+          )}
+        </tbody>
+      </table>
+    </div>
   );
 }
