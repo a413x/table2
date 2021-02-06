5e7142cbc8796ac6288dd5e99c09c4c8de016fb5
Alex <variuhin75@gmail.com>
2021-02-06 13:24:51 +0300

    Add right field to details window

---
+++ src/components/Details/DetailsRight.js
@@ -0,0 +1,30 @@
+import {groupPanels} from './groupPanels.js'
+import '../../styles/DetailsRight.css';
+
+export function DetailsRight({ visits }) {
+  const renderedPanels = groupPanels(visits)
+  return (
+    <div className = 'details-right'>
+      <p className = 'highlight-text'>Мультиканальная цепочка</p>
+      <p>Все посещения за период ретроспективного окна</p>
+      {renderedPanels.map((visitInfo, ind) =>
+        <div  key = {'panel-' + ind} className = 'panel-container'>
+          <div className = 'panel' >
+            <p className = 'highlight-text'>
+              {visitInfo.source + '/' + visitInfo.medium}
+            </p>
+            <p>Source/Medium: {visitInfo.source + '/' + visitInfo.medium}</p>
+            <p>Campaign: {visitInfo.campaign}</p>
+            <p>Content: {visitInfo.campaign}</p>
+            <p>Term: {visitInfo.term}</p>
+            <div className = 'date'>{visitInfo.date}</div>
+            {visitInfo.count > 1 &&
+              <div className = 'count'>{visitInfo.count}</div>
+            }
+          </div>
+          {visitInfo.count > 1 && <div className = 'shadow'></div>}
+        </div>
+      )}
+    </div>
+  );
+}
---
+++ src/components/Details/groupPanels.js
@@ -0,0 +1,41 @@
+import {sortData, convertDate} from '../../utils/utils.js'
+
+function formatPanelFields(visitInfo){
+  const output = {...visitInfo}
+  for(let field in output){
+    if(output[field] === '') {
+      output[field] = '(not set)'
+    }else if(output[field] === 'date'){
+      output[field] = convertDate(output[field])
+    }
+  }
+  return output
+}
+
+export function groupPanels(visits){
+  const output = []
+  const sortedVisits = sortData(visits, 'date', 'desc')
+  const uniq = []
+  const uniqCount = {}
+  //calculate visits count of all types
+  sortedVisits.forEach((visit) => {
+    const mediumSource = visit.source + '/' + visit.medium
+    if(!uniq.includes(mediumSource)){
+      uniq.push(mediumSource)
+      uniqCount[mediumSource] = 1
+    }else{
+      uniqCount[mediumSource] ++
+    }
+  })
+  //build an output with last visit of unique type
+  //and the number of visits of this type
+  uniq.forEach((mediumSource) => {
+    const [source, medium] = mediumSource.split('/')
+    const firstFound = sortedVisits.find((visit) =>
+      visit.source === source && visit.medium === medium
+    )
+    firstFound.count = uniqCount[mediumSource]
+    output.push(formatPanelFields(firstFound))
+  })
+  return output
+}
